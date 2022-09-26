ac48ec3b525834997f38be3b5ea7e9f88c3bded1
muhammad Abdullah <113471263+abdullah-dev5@users.noreply.github.com>
2022-09-26 09:44:03 -0700

    Add files via upload
    Functions using multiple Parameter

---
+++ week_1/Fuinctions.cpp
@@ -0,0 +1,26 @@
+/***
+Functions using multiple parameters.
+
+
+
+**/
+
+
+
+#include<iostream>
+using namespace std;
+
+int addnumbers(int x,int y){
+	int results=x+y;
+	return results;                
+}
+
+int main()
+{
+
+cout<<addnumbers(4,45);
+
+
+return 0;
+}
+
