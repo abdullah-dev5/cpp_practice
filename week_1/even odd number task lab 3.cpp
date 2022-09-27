d4c6869364c0ec6fb503c3310484d153fcab04dc
muhammad Abdullah <113471263+abdullah-dev5@users.noreply.github.com>
2022-09-27 11:38:20 -0700

    Add files via upload
    The Function will show us either the Number is Even or Odd..

---
+++ week_1/even odd number task lab 3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int number;
+cout<< "Enter an Integer number..";
+cin>> number;
+
+if (number/2) {
+
+cout << "the number is even..";
+}
+else {
+	cout <<"the Number is odd..";
+}
+return 0;
+}
+
