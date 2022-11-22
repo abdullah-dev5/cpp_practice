41d73d8269a1e4c5f534f933e0d3a1d2848b5a2e
abdullah-dev5 <arainsaab083@gmail.com>
2022-11-22 18:09:25 +0500

    Create Concept of Integers .cpp

---
+++ C++ practise/Concept of Integers .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//here we are using int data type which is abbrivaition of integers
+//so as we know that integers may have positive or negative digits except zero .  	
+int x=0;
+cout<<"Enter a number "<<endl;
+cin>>x;
+
+if (x){
+	cout<<"True";}
+else {
+	cout<<"False";}
+
+//the output wil be true if entered value is integer
+// and output will be false if entered value is 0 ..
+return 0;
+}
+
